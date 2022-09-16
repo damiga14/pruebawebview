f1ff469c1463d492ec5d7b2b1263baabcd98f950
damiga14 <davidamiga14@gmail.com>
2022-09-16 15:10:25 -0500

    progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -6,9 +6,15 @@ import Iframe from 'react-iframe'
 
 function Home() {
 
-  // useEffect(() => {
-  //   console.log(document.getElementById("iframe_id").contentWindow.location.href)
-  // }, [])
+  let [ruta, setRuta] = useState('https://tutorah.tv')
+
+  function cambia() {
+    document.getElementById("iframe_id").src = 'https://nintendo.com'
+  }
+
+  useEffect(() => {
+    console.log(document.getElementById("iframe_id").src)
+  }, [])
 
 
   return (
@@ -16,25 +22,24 @@ function Home() {
 
       <IonContent fullscreen>
 
-
-
+        <br /><br />
 
         <div className='nav'>
           <IonButton>back</IonButton>
           <IonButton>forward</IonButton>
-          <IonButton>reload</IonButton>
+          <IonButton onClick={() => { cambia() }}>reload</IonButton>
         </div>
 
-        {/* <iframe src="https://tutorah.tv" title="gsoeogle" id='iframe_id' className='frame'></iframe> */}
+        <iframe src="https://tutorah.tv" title="gsoeogle" id='iframe_id' className='frame'></iframe>
 
 
-        <Iframe url="http://tutorah.tv"
+        {/* <Iframe url="http://tutorah.tv"
           width="450px"
           height="450px"
           id="myId"
           className="frame"
           display="initial"
-          position="relative" />
+          position="relative" /> */}
 
       </IonContent>
     </IonPage>
